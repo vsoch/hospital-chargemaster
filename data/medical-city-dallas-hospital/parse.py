5823a5940f894f65ee753f60cc7adc00298734f7
Vanessa Sochat <vsochat@stanford.edu>
2019-02-02 17:38:14 -0500

    this is the worst file here, lol

--- data/medical-city-dallas-hospital/parse.py
+++ data/medical-city-dallas-hospital/parse.py
@@ -2,6 +2,7 @@
 
 import os
 from glob import glob
+from statistics import mean 
 import json
 import pandas
 import datetime
@@ -53,23 +54,63 @@ for result in results:
 
     # service,price,hospital_stay_range\n'
     with open(filename, 'r') as filey:
-        lines = filey.readlines()
+        rows = filey.readlines()
+
 
     # They saved with comma as delimiter, but there are commas in description...
     # /facepalm
-    # This data is not logically parseable.
-    for l in range(1, len(lines)):
-        line = lines[l].strip('\n').split(',')
-        price_range = line[-3]
-        description = ','.join(line[:-3])
-        print(price_range)
-        print(description)
-        content = pandas.read_csv(filename)
+
+    # The first is the header column
+    for row in rows[1:]:
+        comma_split = row.split(",")
+
+        # We don't use this
+        hospital_stay_range = comma_split[-1]  ## assume no commas in last argument
+        with_dollars = [i for i, piece in enumerate(comma_split) if "$" in piece]
+    
+        # Case 1: no price found
+        if len(with_dollars) == 0:
+
+            ## assume price is second-to-last entry
+            price = comma_split[-2]
+
+            ## assume service is everything before first two commas
+            service = "".join(comma_split[:-2])
+
+        elif len(with_dollars) == 1 or len(with_dollars) == 2:
+            first_dollar = min(with_dollars)
+            price = "".join(comma_split[first_dollar: -1])
+            service = "".join(comma_split[:first_dollar])
+
+        else:
+            # more than two dollar signs found in one entry
+            continue
+ 
+        # No price given.
+        if "N/A" in price:
+            continue
+
+        # If the price is a range, take average
+        price = price.replace('$','')
+        if "-" in price:
+            prices = [float(x.strip()) for x in price.split('-')]
+            price = mean(prices)
+        else:
+            price = float(price)
+   
+        idx = df.shape[0] + 1
+        entry = [None,                     # charge code
+                 price,                    # price
+                 service,              # description
+                 result["hospital_id"],    # hospital_id
+                 result['filename'],
+                 charge_type]            
+        df.loc[idx,:] = entry
 
 # Remove empty rows
-#df = df.dropna(how='all')
+df = df.dropna(how='all')
 
 # Save data!
-#print(df.shape)
-#df.to_csv(output_data, sep='\t', index=False)
-#df.to_csv(output_year, sep='\t', index=False)
+print(df.shape)
+df.to_csv(output_data, sep='\t', index=False)
+df.to_csv(output_year, sep='\t', index=False)
