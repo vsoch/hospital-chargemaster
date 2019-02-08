995474714ef32be43904e7d87f87a3ac5869a279
Vanessa Sochat <vsochat@stanford.edu>
2019-02-08 18:13:19 -0500

    adding university of florida

---
+++ data/university-of-florida-health-shands/parse.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import os
+from glob import glob
+import json
+import pandas
+import datetime
+
+here = os.getcwd()
+folder = os.path.basename(here)
+latest = '%s/latest' % here
+year = datetime.datetime.today().year
+
+output_data = os.path.join(here, 'data-latest.tsv')
+output_year = os.path.join(here, 'data-%s.tsv' % year)
+
+# Don't continue if we don't have latest folder
+if not os.path.exists(latest):
+    print('%s does not have parsed data.' % folder)
+    sys.exit(0)
+
+# Don't continue if we don't have results.json
+results_json = os.path.join(latest, 'records.json')
+if not os.path.exists(results_json):
+    print('%s does not have results.json' % folder)
+    sys.exit(1)
+
+with open(results_json, 'r') as filey:
+    results = json.loads(filey.read())
+
+columns = ['charge_code', 
+           'price', 
+           'description', 
+           'hospital_id', 
+           'filename', 
+           'charge_type']
+
+df = pandas.DataFrame(columns=columns)
+
+for result in results:
+    filename = os.path.join(latest, result['filename'])
+    if not os.path.exists(filename):
+        print('%s is not found in latest folder.' % filename)
+        continue
+
+    if os.stat(filename).st_size == 0:
+        print('%s is empty, skipping.' % filename)
+        continue
+
+    charge_type = 'standard'
+    if "drg" in filename.lower():
+        charge_type = "drg"
+
+    print("Parsing %s" % filename)
+
+    if filename.endswith('csv'):
+
+        # ['BILLING_DESC', 'UNIT', 'PRICE']
+        if charge_type == "standard":
+            content = pandas.read_csv(filename, skiprows=4)
+            for row in content.iterrows():
+                idx = df.shape[0] + 1
+                price = row[1]['PRICE'].replace('$','').replace(',','').strip()
+                entry = [None,                         # charge code
+                         price,                        # price
+                         row[1]['BILLING_DESC'],       # description
+                         result["hospital_id"],        # hospital_id
+                         result['filename'],
+                         charge_type]            
+                df.loc[idx,:] = entry
+
+
+        # ['MSDRG', 'Average (Mean) Charges Per Discharge', 'Unnamed: 2']
+        else:
+
+            content = pandas.read_csv(filename, skiprows=3)
+            for row in content.iterrows():
+                idx = df.shape[0] + 1
+                price = row[1]['Average (Mean) Charges Per Discharge'].replace('$','').replace(',','').strip()
+                code = row[1]['MSDRG'].split(' ')[0].strip()
+                entry = [code,                                           # charge code
+                         price, # price
+                         row[1]['MSDRG'],              # description
+                         result["hospital_id"],        # hospital_id
+                         result['filename'],
+                         charge_type]            
+                df.loc[idx,:] = entry
+
+
+# Remove empty rows
+df = df.dropna(how='all')
+
+# Save data!
+print(df.shape)
+df.to_csv(output_data, sep='\t', index=False)
+df.to_csv(output_year, sep='\t', index=False)
